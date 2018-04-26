0066926e396c2a95728748421d94550618ec5f90
VMUSER <paulvav@gmail.com>
2018-04-26 19:57:34 +1000

    Added comments

--- Ass2_Prg2.c
+++ Ass2_Prg2.c
@@ -7,25 +7,29 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+//Memory size for shared memory
 #define SHMSZ 8192
+
+//Name of shared memory - Idenfier
 #define MEM_NAME "shared"
 
+//initialising shared memory id
 int shmid;
-key_t key = 6000;
-double *shm, *s;
+
+//initialising pointer for memory
+double *shm;
 
 
 main()
 {
-
+	//opening memor location - check for errors
 	if ((shmid = shm_open(MEM_NAME,O_RDONLY ,0666)) < 0) 
         	perror("shmget");
   
-
+	//fetching contents of memory and pushing it to shm
 	if ((shm = mmap(0, SHMSZ, PROT_READ, MAP_SHARED, shmid, 0)) == (double *)-1) 
-	{
         	perror("shmat");
-    	}
-	
+ 
+	//print memory contents
 	printf("Last Runtime was: %f seconds\n", *shm);
 }
